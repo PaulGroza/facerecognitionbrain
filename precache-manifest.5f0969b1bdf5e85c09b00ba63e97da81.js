34b6f1b68a6e1ce496118b9ec91f5d6e66d6dc4f
Paul Groza <paul.groza31@gmail.com>
2020-01-31 15:18:15 +0200

    Updates

---
+++ precache-manifest.5f0969b1bdf5e85c09b00ba63e97da81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47646c684ee332e1b7fb9816ffa60fea",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "d119b89435478e187500",
+    "url": "/facerecognitionbrain/static/css/2.1e00258c.chunk.css"
+  },
+  {
+    "revision": "915c59b4bb4873693559",
+    "url": "/facerecognitionbrain/static/css/main.77612d28.chunk.css"
+  },
+  {
+    "revision": "d119b89435478e187500",
+    "url": "/facerecognitionbrain/static/js/2.8481a583.chunk.js"
+  },
+  {
+    "revision": "84f35818fcf031c19f8faeca9494da2d",
+    "url": "/facerecognitionbrain/static/js/2.8481a583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "915c59b4bb4873693559",
+    "url": "/facerecognitionbrain/static/js/main.14a8afac.chunk.js"
+  },
+  {
+    "revision": "466a2f4fa9e4937c0bc5",
+    "url": "/facerecognitionbrain/static/js/runtime-main.a824e37d.js"
+  }
+]);
